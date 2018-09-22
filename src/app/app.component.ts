015898497551b3e5ed93a8d194e76edfa1a3cf02
Martin Dluhos <dluhos.martin@gmail.com>
2018-09-22 07:29:00 +0200

    Set the title of the application and style it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webirs';
+  title = 'WebIRS: mapper for the 21st century';
 }
